aa2e187d2f14ef1aeabb78240d3efb1225bdb3b7
Dohyeon Won <dh1072005@gmail.com>
2025-07-28 23:47:49 +0900

    [UPDATE] API endpoint

--- ui-next/src/lib/api/auth.ts
+++ ui-next/src/lib/api/auth.ts
@@ -1,13 +1,13 @@
 import apiClient from "./api-client";
 
 export function loginWithGoogle() {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/google`;
+    window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/google`;
 }
 
 export async function logout() {
-    return apiClient.post("/auth/logout");
+    return apiClient.post("api/auth/logout");
 }
 
 export async function refreshToken() {
-    return apiClient.post("/auth/refresh");
+    return apiClient.post("api/auth/refresh");
 }
--- ui-next/src/lib/api/users.ts
+++ ui-next/src/lib/api/users.ts
@@ -1,7 +1,7 @@
 import apiClient from "./api-client";
 
 export const fetchCurrentUser = async () => {
-    const response = await apiClient.get("/users/me/info", {
+    const response = await apiClient.get("api/users/me/info", {
         withCredentials: true,
     });
     return response.data;
