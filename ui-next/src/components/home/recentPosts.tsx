62f086f21ec6b5fc8927b52dae79ebcccc583eda
Dohyeon Won <dh1072005@gmail.com>
2025-08-02 17:49:48 +0900

    [UPDATE] tag type

--- ui-next/src/components/home/recentPosts.tsx
+++ ui-next/src/components/home/recentPosts.tsx
@@ -2,25 +2,7 @@ import { PostCard } from "../post/postCard";
 import styles from "./recentPosts.module.css";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-
-interface Author {
-    name: string;
-    avatar: string;
-    bio: string;
-}
-interface Post {
-    id: number;
-    title: string;
-    subtitle: string;
-    excerpt: string;
-    author: Author;
-    publishedAt: string;
-    readTime: number;
-    tags: string[];
-    featured: boolean;
-    claps: number;
-    image: string;
-}
+import { Post } from "@/lib/types/post.interface";
 
 export function RecentPosts() {
     const [posts, setPosts] = useState<Post[]>([]);
